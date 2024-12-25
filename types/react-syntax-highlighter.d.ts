3a821940a2c1e5f4efcba059754e2b6098ea1338
Muhammad Zaki <162775830+mdzakidev@users.noreply.github.com>
2024-12-25 19:12:33 +0700

    Update react-syntax-highlighter.d.ts

--- types/react-syntax-highlighter.d.ts
+++ types/react-syntax-highlighter.d.ts
@@ -1,14 +1,29 @@
 declare module 'react-syntax-highlighter' {
-  import React from 'react';
-  
-  export const Prism: React.ComponentType<{
-    children: string;
-    language: string;
-    style?: any;
-  }>;
+  import React from 'react'
+  export const PrismLight: React.ComponentType<{
+    children: string
+    style?: any
+    language?: string
+    showLineNumbers?: boolean
+  }>
 }
 
 declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
-  export const atomDark: any;
+  export const atomDark: any
+}
+
+declare module 'react-syntax-highlighter/dist/esm/languages/prism/bash' {
+  const language: any
+  export default language
+}
+
+declare module 'react-syntax-highlighter/dist/esm/languages/prism/javascript' {
+  const language: any
+  export default language
+}
+
+declare module 'react-syntax-highlighter/dist/esm/languages/prism/json' {
+  const language: any
+  export default language
 }
 
